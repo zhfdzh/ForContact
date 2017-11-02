c6ad3a66428d0d87b395c1efc3692510fcc7ab8d
zhuhf3 <zhuhf3@lenovo.com>
2017-11-02 16:54:30 +0800

    1. add baidu location SDK 2. add DrawerLayout and mainActivity etc. 3. add GlobleVariable , ActionbarUtil class 4. add some xml file for mainActivity

--- app/src/main/java/com/example/zhf/forcontact/LoginActivity.java
+++ app/src/main/java/com/example/zhf/forcontact/LoginActivity.java
@@ -1,74 +1,192 @@
 package com.example.zhf.forcontact;
 
+import android.app.Activity;
+import android.content.*;
 import android.os.*;
-import android.support.v4.view.*;
-import android.support.v7.app.*;
+import android.text.*;
+import android.util.*;
 import android.view.*;
+import android.widget.*;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.zhf.forcontact.util.*;
 
-    private ActionBar mActionBar;
+
+public class LoginActivity extends Activity implements View.OnClickListener,TextWatcher{
+
+    private android.app.ActionBar mActionBar;
+    private EditText    mUserName;
+    private EditText    mPassword;
+    private Button      mlogin;
+    private ImageView   mQQLogin;
+    private ImageView   mWechatLogin;
+    private ImageView   mWeiboLogin;
+    private SharedPreferences mSharepreferenced;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        mActionBar = getSupportActionBar();
+        mActionBar = getActionBar();
         if (mActionBar != null) {
-            mActionBar.hide();
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+            mActionBar.setDisplayShowHomeEnabled(false);              //设置显示actionbar的返回按钮
+//            mActionBar.setDisplayShowTitleEnabled(false);            //设置显示actionbar的图标
+            mActionBar.setTitle(getResources().getString(R.string.back));
         }
-        setStatusBarUpperAPI21();
+        ActionBarUtil.setStatusBarUpper(this);
+        setContentView(R.layout.activity_login);
+        initView();
+
+        setLoginListener();
+    }
+    private void initView() {
+        mUserName = findViewById(R.id.userName);
+        mPassword = findViewById(R.id.password);
+        mlogin = findViewById(R.id.app_login);
+        mQQLogin = findViewById(R.id.qq_login);
+        mWechatLogin = findViewById(R.id.wecaht_login);
+        mWeiboLogin = findViewById(R.id.weibo_login);
+        Log.d("LoginActivity","" + (mQQLogin == null));
+
+        mSharepreferenced = getSharedPreferences(GlobleVariable.USER_PASSWORD_SHAREAPREFERENCES,MODE_PRIVATE);
+        String username = mSharepreferenced.getString(GlobleVariable.USERNAME,"");
+        String password = mSharepreferenced.getString(GlobleVariable.PASSWORD,"");
+        mUserName.setText(username);
+        mPassword.setText(password);
     }
 
-    /**
-     * 全屏模式
-     * */
-    private void setStatusBarUpper() {
-        Window window = getWindow();
-        //设置透明状态栏,这样才能让 ContentView 向上
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    private void setLoginListener() {
+        mlogin.setOnClickListener(this);
+        mQQLogin.setOnClickListener(this);
+        mWechatLogin.setOnClickListener(this);
+        mWeiboLogin.setOnClickListener(this);
+        mUserName.addTextChangedListener(this);
+        mPassword.addTextChangedListener(this);
 
-        //需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+    }
 
-        ViewGroup mContentView = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
-        View mChildView = mContentView.getChildAt(0);
-        if (mChildView != null) {
-            //注意不是设置 ContentView 的 FitsSystemWindows, 而是设置 ContentView 的第一个子 View . 使其不为系统 View 预留空间.
-            ViewCompat.setFitsSystemWindows(mChildView, false);
+    @Override
+    public void onClick(View v) {
+        Log.d("LoginActivity","enter onClick");
+
+        switch(v.getId()){
+            case R.id.app_login:
+                String user = mUserName.getText().toString();
+                String pass = mPassword.getText().toString();
+
+                if(user.equals("") || pass.equals("")){
+                    Toast.makeText(this,"用户名密码不能为空",Toast.LENGTH_SHORT).show();
+                }else {
+                    if(pass.length() < GlobleVariable.PASSWORD_MINLENGTH){
+                        Toast.makeText(this,"密码长度不能低于6位",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Log.d("LoginActivity","user: "+ user + " --- pass: " + pass);
+                    UserLoginTask loginTask = new UserLoginTask(user,pass);
+                    loginTask.execute();
+                    mlogin.setEnabled(false);
+                }
+                break;
+            case R.id.wecaht_login:
+
+                break;
+            case R.id.weibo_login:
+
+                break;
+            case R.id.qq_login:
+
+                break;
         }
     }
 
-    /**
-     * 着色模式
-     * */
-    private void setStatusBarUpperAPI21(){
-        Window window = getWindow();
-        //取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        //需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        //设置状态栏颜色
-        //由于setStatusBarColor()这个API最低版本支持21, 本人的是15,所以如果要设置颜色,自行到style中通过配置文件设置
-//        window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
-        ViewGroup mContentView = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
-        View mChildView = mContentView.getChildAt(0);
-        if (mChildView != null) {
-            //注意不是设置 ContentView 的 FitsSystemWindows, 而是设置 ContentView 的第一个子 View . 预留出系统 View 的空间.
-            ViewCompat.setFitsSystemWindows(mChildView, true);
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        Log.d("LoginActivity","enter onEditorAction");
+
+        String user = mUserName.getText().toString();
+        String pass = mPassword.getText().toString();
+        if( !(user.equals("")) && !(pass.equals(""))){
+            mlogin.setEnabled(true);
+            mlogin.setBackground(getDrawable(R.drawable.login_button_select_shape));
+
+        }else {
+            mlogin.setEnabled(false);
+            mlogin.setBackground(getDrawable(R.drawable.login_button_style_gray));
         }
     }
 
-    /**
-     * 获取状态栏高度
-     * */
-    private int getStatusBarHeight(){
-        int result = 0;
-        int resId = getResources().getIdentifier("status_bar_height","dimen","android");
-        if(resId>0){
-            result = getResources().getDimensionPixelSize(resId);
+    private class UserLoginTask extends AsyncTask<Void, Void, Boolean>{
+
+        String userNameString;
+        String passWordString;
+
+        UserLoginTask(String userName, String passWord){
+            this.userNameString = userName;
+            this.passWordString = passWord;
         }
-        return result;
+
+        @Override
+        protected void onPreExecute() {      // 异步任务执行前执行，做一些初始化任务
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void[] params) {
+            Log.d("LoginActivity","enter doInBackground");
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+                if(userNameString.equals("zhuhf") && passWordString.equals("123456")){
+                    return true;
+                }
+            } catch (InterruptedException e) {
+                return false;
+            }
+            return false;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void[] values) {       //在调用publishProgress(Progress... values)时，此方法被执行，直接将进度信息更新到UI组件上。
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean o) {       // 当后台操作结束时，此方法将会被调用
+            Log.d("LoginActivity","enter onPostExecute");
+            super.onPostExecute(o);
+            if(o){
+                Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_LONG).show();
+                SharedPreferences.Editor editor = mSharepreferenced.edit();
+                editor.putString(GlobleVariable.USERNAME,userNameString);
+                editor.putString(GlobleVariable.PASSWORD,passWordString);
+                editor.apply();
+            }else {
+                Toast.makeText(LoginActivity.this,"登录失败",Toast.LENGTH_LONG).show();
+            }
+            mlogin.setEnabled(true);
+        }
+
+        @Override
+        protected void onCancelled() {     // 方法用于在取消执行中的任务时更改UI
+            super.onCancelled();
+        }
     }
 }
---
+++ app/src/main/java/com/example/zhf/forcontact/util/GlobleVariable.java
@@ -0,0 +1,12 @@
+package com.example.zhf.forcontact.util;
+
+/**
+ * Created by zhf on 2017/11/2.
+ */
+
+public class GlobleVariable {
+    public static final String USER_PASSWORD_SHAREAPREFERENCES = "USER_PASSWORD";
+    public static final String USERNAME = "USERNAME";
+    public static final String PASSWORD = "PASSWORD";
+    public static final int PASSWORD_MINLENGTH = 6;
+}
